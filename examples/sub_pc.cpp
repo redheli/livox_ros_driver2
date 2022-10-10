9848c16063bb6ae0fd015d5524520f7ca243a41d
Double Max <max@ground-map.com>
2022-10-10 21:43:07 +0800

    read topic from argument

--- examples/sub_pc.cpp
+++ examples/sub_pc.cpp
@@ -23,11 +23,17 @@ void pcCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
 
 int main(int argc, char** argv)
 {
+    std::string pointcloud2_topic = "/livox/lidar";
+    if (argc > 1){
+        pointcloud2_topic = std::string(argv[1]);
+    }
+    ROS_INFO("Point Cloud2 Topic <%s>",pointcloud2_topic.c_str());
+
     ros::init(argc, argv, "sub_pc");
 
     ros::NodeHandle n;
 
-    ros::Subscriber sub = n.subscribe("/livox/lidar", 1000, pcCallback);
+    ros::Subscriber sub = n.subscribe(pointcloud2_topic, 1000, pcCallback);
 
     ros::spin();
 
